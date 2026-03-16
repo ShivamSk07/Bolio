966991e5a4e6ba4891e3071fbea24fbdea9ea81d
ShivamSk07 <skfatmosphere@gmail.com>
2026-03-16 21:23:49 +0530

    first commit

--- bolio_core/asgi.py
+++ bolio_core/asgi.py
@@ -1,25 +1,17 @@
-"""
-ASGI config for bolio_core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+
+# Initialize Django ASGI application early to ensure the settings are loaded
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bolio_core.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
-from channels.security.websocket import AllowedHostsOriginValidator
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bolio_core.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
--- bolio_core/settings.py
+++ bolio_core/settings.py
@@ -176,6 +176,8 @@ STORAGES = {
     },
 }
 
+WHITENOISE_MANIFEST_STRICT = False
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
